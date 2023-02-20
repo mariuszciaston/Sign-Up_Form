7fd98ad88989dfce2e138ba51bfcc2b47b65ff65
Mariusz <casmar90@gmail.com>
2023-02-20 09:43:34 +0100

    Fix layout scroll, password validation in progress

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const confirmPassword = document.querySelector('#confirm-password');
 const phoneNumber = document.querySelector('#phone-number');
+const confirmPassword = document.querySelector('#confirm-password');
 
 const emailError = document.querySelector('#email-error');
 const phoneNumberError = document.querySelector('#phone-number-error');
@@ -11,17 +11,57 @@ email.addEventListener("focusout", () => {
 });
 
 phoneNumber.addEventListener("focusout", () => {
-    phoneNumber.validity.tooShort ? phoneNumberError.classList.add('show') : phoneNumberError.classList.remove('show');
+    if (phoneNumber.validity.patternMismatch) {
+        phoneNumberError.classList.add('show')
+    }
 });
 
-// password.addEventListener("input", () => {
-//     password.validity.patternMismatch ? passwordError.classList.add('show') : passwordError.classList.remove('show');
+phoneNumber.addEventListener("input", () => {
+    if (phoneNumber.validity.patternMismatch === false) {
+        phoneNumberError.classList.remove('show')
+    };
+});
+
+
+
+password.addEventListener("focus", () => {
+    passwordError.classList.add('show');
+});
+
+password.addEventListener("focusout", () => {
+    if (password.validity.patternMismatch === false) {
+        passwordError.classList.remove('show');
+    }
+});
+
+confirmPassword.addEventListener("focusout", () => {
+    if (password.value !== confirmPassword.value) {
+        confirmPasswordError.classList.add('show');
+    }
+});
+
+confirmPassword.addEventListener("input", () => {
+    if (password.value === confirmPassword.value) {
+        confirmPasswordError.classList.remove('show')
+    }
+});
+
+
+
+// SUBMIT
+
+// confirmPassword.addEventListener("input", () => {
+
+//     if (((password.validity.patternMismatch) && (confirmPassword.validity.patternMismatch)) && (password.value === confirmPassword.value)) {
+//         alert(password.value + ' password match ' + confirmPassword.value);
+//     }
+//     else {
+//         alert('passwords do not match');
+//     }
 // });
+
+
 
 // confirmPassword.addEventListener("input", () => {
 //     confirmPassword.validity.patternMismatch ? confirmPasswordError.classList.add('show') : confirmPasswordError.classList.remove('show');
 // });
-
-
-
-
